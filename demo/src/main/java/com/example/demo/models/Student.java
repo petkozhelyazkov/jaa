c85fdcddef448ad1508f471104d5c3c5553fdb4f
Petko Zhelyazkov <petkozheliazkov1@gmail.com>
2025-01-10 01:29:51 +0200

    Create Student model

---
+++ demo/src/main/java/com/example/demo/models/Student.java
@@ -0,0 +1,28 @@
+package com.example.demo.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "student")
+@Data
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @JsonIgnore
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "faculty_number")
+    private Integer facultyNumber;
+
+    @Column(name = "email")
+    private String email;
+}
