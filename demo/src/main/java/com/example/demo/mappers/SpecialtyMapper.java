779d57ee8eeeb51818d8063ad3c65f219ff817f4
PanayotAsparuhov <pani66@abv.bg>
2025-01-10 01:57:41 +0200

    Add SpecialtyMapper

---
+++ demo/src/main/java/com/example/demo/mappers/SpecialtyMapper.java
@@ -0,0 +1,24 @@
+package com.example.demo.mappers;
+
+import com.example.demo.dto.SpecialtyDto;
+import com.example.demo.models.Specialty;
+
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface SpecialtyMapper {
+
+    @Mapping(target = "name", source = "dto.name")
+    @Mapping(target = "faculty.id", source = "dto.facultyId")
+    Specialty convertDtoToEntity(SpecialtyDto dto, Long id);
+
+    Specialty convertDtoToEntity(SpecialtyDto dto);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    Specialty updateSpecialtyFromDto(SpecialtyDto dto, @MappingTarget Specialty entity);
+}
